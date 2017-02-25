3258833d78016f62e38151c0449feff1ab08d3ae
icygub <jchubbs95@gmail.com>
2017-02-25 01:02:05 -0700

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -0,0 +1,10 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by icyhot on 25/02/2017.
+ */
+
+public class CrimeFragment extends Fragment  {
+}
